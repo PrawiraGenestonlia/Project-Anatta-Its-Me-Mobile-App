339f60595c54d51be366dd9309525997de098f54
PrawiraGenestonlia <prawira.96@gmail.com>
2019-09-30 13:23:50 +0800

    .

--- app/screens/globalstylesheet.stylesheet.js
+++ app/screens/globalstylesheet.stylesheet.js
@@ -2,6 +2,7 @@ import {
   StyleSheet,
 } from 'react-native';
 
+//this is a global stylesheet but it has to be included
 const styles = StyleSheet.create({
   container: {
     marginVertical: 16,
